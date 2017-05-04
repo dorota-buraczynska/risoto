62fa13eede8c9b3f11b272535f4f30663bfb2c23
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-05-04 21:10:02 +0200

    add slide for small screen

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 $('.nav__menu-item').on('click', function (event) {
     event.preventDefault();
     var itemaActiveClass = 'nav__menu-item--active';
-   $(this)
-       .addClass(itemaActiveClass)
-       .siblings().removeClass(itemaActiveClass);
+    $(this)
+        .addClass(itemaActiveClass)
+        .siblings().removeClass(itemaActiveClass);
 });
 
 $('.nav__button').on('click', function (event) {
@@ -12,4 +12,38 @@ $('.nav__button').on('click', function (event) {
     var buttonActiveClass = 'nav__button--active';
     $(this).toggleClass(buttonActiveClass);
     $('.nav__menu-list').toggleClass('visible');
+});
+
+//slider
+var activeClass = 'slider__item--active';
+var activeIndexPhoto = $('.slider__item--active').index();
+var nextPhotoIndex;
+var slide = $('.slider__item');
+
+$('.slider__button--left').on('click', function () {
+    slide.eq(activeIndexPhoto - 1).removeClass(activeClass);
+
+    if (activeIndexPhoto === 1) {
+        activeIndexPhoto = slide.length;
+        nextPhotoIndex = slide.length;
+    } else {
+        nextPhotoIndex = activeIndexPhoto - 1;
+        activeIndexPhoto--;
+    }
+
+    slide.eq(nextPhotoIndex - 1).addClass(activeClass);
+});
+
+$('.slider__button--right').on('click', function () {
+    slide.eq(activeIndexPhoto - 1).removeClass(activeClass);
+
+    if (activeIndexPhoto === slide.length) {
+        activeIndexPhoto = 1;
+        nextPhotoIndex = 1;
+    } else {
+        nextPhotoIndex = activeIndexPhoto + 1;
+        activeIndexPhoto++;
+    }
+
+    slide.eq(nextPhotoIndex - 1).addClass(activeClass);
 });
